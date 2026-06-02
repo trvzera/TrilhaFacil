2f24e464d4613742503674007163eb5eef0c4150
GIOVANNI_DE_CASTRO_MORAIS_TRIVELLATO <22400087@SIGMAB30.BARROCA>
2026-06-02 11:49:17 -0300

    Commit final da aula

--- app/src/main/java/com/example/trilhafacil/MainActivity.kt
+++ app/src/main/java/com/example/trilhafacil/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.trilhafacil
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val txtTitulo = findViewById<TextView>(R.id.textView)
+        val editNomeTrilha = findViewById<EditText>(R.id.editTextText)
+        val btnPlanejar = findViewById<Button>(R.id.button3)
+
+        btnPlanejar.setOnClickListener {
+            val nomeTrilha = editNomeTrilha.text.toString().trim()
+
+            if (nomeTrilha.isEmpty()) {
+                Toast.makeText(this, "Por favor, digite o nome de uma trilha!", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                txtTitulo.text = "Próxima aventura: $nomeTrilha!"
+
+                Toast.makeText(
+                    this,
+                    "Trilha '$nomeTrilha' planejada com sucesso! 🥾",
+                    Toast.LENGTH_LONG
+                ).show()
+
+                editNomeTrilha.text.clear()
+            }
         }
     }
 }
